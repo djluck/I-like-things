6ce3bcc174517e7a385308bafc0af9bdc056f05c
unknown <Jimmy@Fatman.(none)>
2012-03-16 13:02:18 +0000

    init

--- Handlers/list.py
+++ Handlers/list.py
@@ -1,12 +1,19 @@
 import webapp2
 from handlers.base import Base
 from google.appengine.ext import db
+from entities.entry import Entry
+from google.appengine.api import users
 
 class List(Base):
     def get(self):
+        q = Entry.all();
+        q.filter("user = ", users.get_current_user())
+        q.order("-date_created")
         template_values = {
-            "name":"james"
+            "user": users.get_current_user(),
+            "entries" : q.fetch(10)
         }
+        
         self.render_template("list.html",  template_values)
 
 app = webapp2.WSGIApplication([('/.*', List)],
