ef325f2de74de90c4f057c230f5f17435254fdf9
djluck <djluck20@googlemail.com>
2012-03-19 20:36:00 +0000

    Init

--- Handlers/add.py
+++ Handlers/add.py
@@ -3,32 +3,54 @@ from handlers.base import Base
 from entities.entry import Entry
 from google.appengine.ext import db
 from datetime import datetime
+import urlparse
 
 class Add(Base):
     def get(self):
         self.render_initial()
         
     def post(self):
-        link = self.request.get("link")
-        if link[0:4] != "http":
+        errors = []
+        
+        link = self.request.get("link").strip(" ")
+        if link[0:4] != "http" and len(link) > 0:
             link = "http://" + link
+        if len(link) == 0:
+            errors.append("Link must not be empty")
+        elif reduce(lambda x, y: len(y) > 0 and x, urlparse.urlparse(link), True):
+            errors.append("Link must be a valid url")
+        
         date_expires = self.request.get("date_expires")
         if date_expires != "":
             date_expires = datetime.strptime(date_expires, "%d/%m/%y").date()
         else:
             date_expires = None
-        tags = [db.Category(x) for x in self.request.get("tags").split(" ")]
-        entry = Entry(link=link, 
-                      tags = tags,
-                      date_expires = date_expires)
-        entry.put()
+            
+        tags_raw = self.request.get("tags").strip(" ").split(" ")
+        if len(tags_raw) == 1 and tags_raw[0] == "":
+            errors.append("You must enter at least 1 tag")
+        else:
+            tags = [db.Category(x) for x in tags_raw]
         
-        self.render_initial(additional_values={"added" : True, "link" : entry.link})
+        if len(errors) == 0:
+            entry = Entry(link=link, 
+                          tags = tags,
+                          date_expires = date_expires)
+            entry.put()
+            self.redirect("?added=True")
+        else:
+            self.render_initial({"errors": errors})
         
-    def render_initial(self, additional_values=None):
+        
+        
+    def render_initial(self, additional_values={}):
         result =  Entry.all().fetch(100)
         tags = [i for i in set([t for e in result for t in e.tags])]
-        self.render_template("add.html", {"tags" : tags})  
+        template_values = {"tags" : tags}
+        template_values = dict(template_values.items() + additional_values.items())
+        if (self.request.get("added") != ""):
+            template_values["added"] = True
+        self.render_template("add.html", template_values)  
         
         
 app = webapp2.WSGIApplication([('/new', Add)],
