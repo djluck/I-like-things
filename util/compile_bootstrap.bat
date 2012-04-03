d5eaa651543c7c72659cf49987f01328c481c28e
unknown <Jimmy@Fatman.(none)>
2012-04-03 18:58:24 +0100

    Init

---
+++ util/compile_bootstrap.bat
@@ -0,0 +1,1 @@
+node lessc.js ..\content\styles\bootstrap\bootstrap.less -x > ..\content\styles\bootstrap\bootstrap.css
---
+++ util/watch_bootstrap.bat
@@ -0,0 +1,1 @@
+watchmedo shell-command -p="*.less" -c="compile_bootstrap.bat" ..\content\styles\bootstrap
